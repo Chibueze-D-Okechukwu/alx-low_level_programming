05c14aa21d638b37442f9aa45b20f932b7faac86
Chibueze-D-Okechukwu <chibuezeokechukwu23@gmail.com>
2023-02-22 18:29:59 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Prints natural numbers below 1024 that are
  * multiplies of 3 or 5
@@ -10,13 +9,12 @@
 
 int main(void)
 {
-        int a, b;
-
-        for (a = 1; a < 1024; a++)
-        {
-                if ((a % 3) == 0 || (a % 5) == 0)
-                        b += a;
-        }
-        printf("%d\n", b);
-        return (0);
+	int a, b;
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
