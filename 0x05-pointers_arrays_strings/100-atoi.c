8cd5248be5045baeb8291a28632af4711b5db409
Chibueze-D-Okechukwu <chibuezeokechukwu23@gmail.com>
2023-03-09 16:33:11 -0800

    Correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _atoi - converts a string to an interger
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
@@ -35,8 +36,11 @@ int _atoi(char *s)
 				break;
 			f = 0;
 		}
+		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
